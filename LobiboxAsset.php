e6ddc045ec105af5ef07a0bbb96efd10802a35ab
Surakit Choodet <kuakling@gmail.com>
2016-12-11 16:22:33 +0700

    Create LobiboxAsset.php

---
+++ LobiboxAsset.php
@@ -0,0 +1,19 @@
+<?php
+namespace kuakling\lobibox;
+use yii\web\AssetBundle;
+/**
+ * Asset bundle for the Lobibox files.
+ *
+ * @author kuakling <kuakling@gmail.com>
+ * @since 2.0
+ */
+class LobiboxAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/lobibox/dist';
+    public $css = [
+        'css/lobibox.min.css',
+    ];
+    public $js = [
+        'js/lobibox.min.js',
+    ];
+}
