b2eef125ee1653ff854ba2165127cf95dafe5a7a
Surakit Choodet <kuakling@gmail.com>
2016-12-11 16:23:21 +0700

    Update Notification.php

--- Notification.php
+++ Notification.php
@@ -11,11 +11,43 @@ use yii\helpers\Html;
  */
 class Notification extends \yii\base\Widget
 {   
+    public $type = 'default';
+    
+    public $title = null;
+    
+    public $msg = '';
+    
+    public $options = [];
+    
+    public function init()
+    {
+        parent::init();
+        
+        if(is_null($this->title)){ $this->title = ucfirst($this->type); }
+    }
     /**
      * Renders the widget.
      */
     public function run()
     {
-        LoliboxAsset::register($this->getView());
+        $asset = LobiboxAsset::register($this->getView());
+        $this->registerNoti($asset);
+    }
+    
+    public function registerNoti($asset)
+    {
+        $view = $this->getView();
+        $type = $this->type;
+        $options = array_merge([
+            'msg' => $this->msg,
+            'title' => $this->title,
+            'position' => 'right top',
+            'soundPath' => $asset->baseUrl.'/sounds/'
+        ], $this->options);
+        $jsonOptions = \yii\helpers\Json::encode($options);
+        $js[] = <<< JS
+Lobibox.notify('$this->type', $jsonOptions);
+JS;
+        $view->registerJs(implode("\n", $js));
     }
 }
